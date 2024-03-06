43b6cace40a8768a53e4c601748554c145c8c879
bomsk <112770710+Balzhan31@users.noreply.github.com>
2024-03-06 18:14:37 +0600

    example

---
+++ store/src/app/product.ts
@@ -0,0 +1,105 @@
+export interface Product {
+    id: number;
+    name: string;
+    price: number;
+    description: string;
+    rating: number;
+    img: string;
+    link: string;
+  }
+  
+  export const products = [
+    {
+      id: 1,
+      name: 'Romand Glasting Melting Balm 06 Kaya Fig',
+      price: 3500,
+      description: 'блеск для губ розовый',
+      rating: 4.5,
+      img: 'https://resources.cdn-kaspi.kz/img/m/p/h0d/h2b/80956703768606.jpg?format=gallery-large',
+      link: 'https://kaspi.kz/shop/p/romand-glasting-melting-balm-06-kaya-fig-blesk-dlja-gub-rozovyi-110576624/?c=750000000'
+    },
+    {
+      id: 2,
+      name: 'Romand Glasting Melting Balm 12 Veiled Rose',
+      price: 4989,
+      description: 'Romand Glasting Melting Balm бальзам оттеночный для губ розовый 12 Veiled Rose',
+      rating: 4.1,
+      img: 'https://resources.cdn-kaspi.kz/img/m/p/hc7/hfe/84541687529502.jpg?format=gallery-large',
+      link: 'https://kaspi.kz/shop/p/romand-glasting-melting-balm-bal-zam-ottenochnyi-dlja-gub-rozovyi-12-veiled-rose-114754554/?c=750000000'
+    },
+    {
+      id: 3,
+      name: 'Romand Bare Water Cushion 21 Natural',
+      price: 6999,
+      description: 'кушон 20 мл',
+      rating: 4.9,
+      img: 'https://resources.cdn-kaspi.kz/img/m/p/hdf/h00/69240339496990.jpg?format=gallery-medium',
+      link: 'https://kaspi.kz/shop/p/romand-bare-water-cushion-kushon-21-natural-20-ml-108989094/?c=750000000'
+    },
+    {
+      id: 4,
+      name: 'Romand Dewyful Water Tint 12 Canyonг',
+      price: 4199,
+      description: 'тинт для губ коричневый',
+      rating: 5.0,
+      img: 'https://resources.cdn-kaspi.kz/img/m/p/h39/h1f/67477117501470.jpg?format=gallery-large',
+      link: 'https://kaspi.kz/shop/p/romand-dewyful-water-tint-tint-dlja-gub-korichnevyi-12-canyon-108288762/?c=750000000'
+    },
+    {
+      id: 5,
+      name: 'Romand Glasting Melting Balm 13 Scotch Nude',
+      price: 4989,
+      description: 'бальзам оттеночный для губ коричневый',
+      rating: 4.5,
+      img: 'https://resources.cdn-kaspi.kz/img/m/p/hd9/h59/84541249028126.jpg?format=gallery-medium',
+      link: 'https://kaspi.kz/shop/p/romand-glasting-melting-balm-bal-zam-ottenochnyi-dlja-gub-korichnevyi-13-scotch-nude-114754545/?c=750000000'
+    },
+    {
+      id: 6,
+      name: 'Romand 25 BARE GRAPE',
+      price: 4399,
+      description: 'тинт для губ красный',
+      rating: 4.5,
+      img: 'https://resources.cdn-kaspi.kz/img/m/p/h0c/h1d/67413378039838.jpg?format=gallery-large',
+      link: 'https://kaspi.kz/shop/p/romand-tint-dlja-gub-krasnyi-25-bare-grape-108262862/?c=750000000'
+    }, {
+      id: 7,
+      name: 'Romand Glasting Color Gloss 05 Dim Mauve',
+      price: 5799,
+      description: 'блеск для губ коричневый',
+      rating: 4.5,
+      img: 'https://resources.cdn-kaspi.kz/img/m/p/h6a/h9f/84798398169118.png?format=gallery-medium',
+      link: 'https://kaspi.kz/shop/p/romand-glasting-color-gloss-blesk-dlja-gub-korichnevyi-05-dim-mauve-115704462/?c=750000000'
+    }, {
+      id: 8,
+      name: 'Romand Better Than Cheek C02 Blueberry Chi',
+      price: 4990,
+      description: 'румяна',
+      rating: 4.8,
+      img: 'https://resources.cdn-kaspi.kz/img/m/p/h07/h9c/81155452108830.jpg?format=gallery-medium',
+      link: 'https://kaspi.kz/shop/p/romand-better-than-cheek-rumjana-c02-blueberry-chip-110743307/?c=750000000'
+    }, {
+      id: 9,
+      name: 'Romand Better Than Palette 03 Rosebud Garden',
+      price: 12300,
+      description: 'тени прессованные',
+      rating: 5.0,
+      img: 'https://resources.cdn-kaspi.kz/img/m/p/h4c/had/67910833700894.jpg?format=gallery-medium',
+      link: 'https://kaspi.kz/shop/p/romand-teni-pressovannye-better-than-palette-03-rosebud-garden-108454535/?c=750000000'
+    }, {
+      id: 10,
+      name: 'Romand The Universe 06 little meteor',
+      price: 4990,
+      description: 'глиттер жидкие',
+      rating: 4.9,
+      img: 'https://resources.cdn-kaspi.kz/img/m/p/he4/h09/85016513183774.jpg?format=gallery-medium',
+      link: 'https://kaspi.kz/shop/p/romand-glitter-zhidkie-the-universe-06-little-meteor-116221997/?c=750000000'
+    }
+  ];
+  
+  
+  /*
+  Copyright Google LLC. All Rights Reserved.
+  Use of this source code is governed by an MIT-style license that
+  can be found in the LICENSE file at https://angular.io/license
+  */
