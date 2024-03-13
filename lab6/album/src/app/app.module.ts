15031912b3805c54c1a3916e89b4e9f6ab846cf4
bomsk <112770710+Balzhan31@users.noreply.github.com>
2024-03-13 14:11:41 +0600

    wayo

---
+++ lab6/album/src/app/app.module.ts
@@ -0,0 +1,9 @@
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  imports: [
+    // ... other imports
+    RouterModule.forRoot([]), // Add your route configurations inside the parentheses
+  ],
+  // ...
+})
