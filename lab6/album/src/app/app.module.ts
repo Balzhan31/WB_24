97d70aee7e8932bce4dbf58b81a27182f16c5cab
bomsk <112770710+Balzhan31@users.noreply.github.com>
2024-03-13 16:00:12 +0600

    Delete lab6/album directory

--- lab6/album/src/app/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { RouterModule } from '@angular/router';
-
-@NgModule({
-  imports: [
-    // ... other imports
-    RouterModule.forRoot([]), // Add your route configurations inside the parentheses
-  ],
-  // ...
-})
