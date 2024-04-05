0ec8d2b5c1c2e328372fb46f984436fb98ccaa0a
bomsk <112770710+Balzhan31@users.noreply.github.com>
2024-04-05 14:11:49 +0500

    Update 1.py

--- lab7/task1/codingbat/list-1/1.py
+++ lab7/task1/codingbat/list-1/1.py
@@ -1,5 +1,5 @@
 '''
-Given an array of ints, return True if 6 appears as either the 
+Given array of ints, return True if 6 appears as either the 
 first or last element in the array. The array will be length 1 or more.
 '''
 def first_last6(nums):
